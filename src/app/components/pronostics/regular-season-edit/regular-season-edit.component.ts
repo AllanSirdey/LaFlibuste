a762bd44cff34c383d22f7a85c3d8b7ff19b19e5
sirdey <allan.sirdey@laposte.net>
2021-10-14 23:10:09 +0200

    Vote pour la saison reguliere ok

--- src/app/components/pronostics/regular-season-edit/regular-season-edit.component.ts
+++ src/app/components/pronostics/regular-season-edit/regular-season-edit.component.ts
@@ -22,14 +22,13 @@ export class RegularSeasonEditComponent implements OnInit {
   teamsEast: Team[] = [];
 
   /* 15 équipes de la conférences OUEST */
-  teamWest: Team[] = [];
+  teamsWest: Team[] = [];
 
   /*Endpoint équipes NBA */
   private teamsUrl = 'http://data.nba.net/data/10s/prod/v1/2021/teams.json';
 
   /* Formulaire réactif */
-  formTeamEast: FormGroup;
-  formTeamWest: FormGroup;
+  formTeams: FormGroup;
 
   /* Constructeur */
   constructor(
@@ -44,6 +43,7 @@ export class RegularSeasonEditComponent implements OnInit {
     this.utilisateursConnecteSubscription = this.userService.utilisateurConnecteSubject.subscribe(
       (utilisateurConnecte: User) => {
         this.user = utilisateurConnecte;
+        this.updateValue();
       }
     );
     this.userService.emitUtilisateurConnecte();
@@ -55,61 +55,167 @@ export class RegularSeasonEditComponent implements OnInit {
     this.getTeamsFromApi();
   }
 
-  ngAfterContentInit() {
+  ngAfterViewInit() {
     this.updateValue();
   }
 
   initForm() {
-    this.formTeamEast = this.formBuilder.group({
-      team1: [''],
-      team2: [''],
-      team3: [''],
-      team4: [''],
-      team5: [''],
-      team6: [''],
-      team7: [''],
-      team8: ['']
+    this.formTeams = this.formBuilder.group({
+      // teams East
+      teamEast1: [''],
+      teamEast2: [''],
+      teamEast3: [''],
+      teamEast4: [''],
+      teamEast5: [''],
+      teamEast6: [''],
+      teamEast7: [''],
+      teamEast8: [''],
+      teamEast9: [''],
+      teamEast10: [''],
+      teamEast11: [''],
+      teamEast12: [''],
+      teamEast13: [''],
+      teamEast14: [''],
+      teamEast15: [''],
+      teamEast16: [''],
+      // teams West
+      teamWest1: [''],
+      teamWest2: [''],
+      teamWest3: [''],
+      teamWest4: [''],
+      teamWest5: [''],
+      teamWest6: [''],
+      teamWest7: [''],
+      teamWest8: [''],
+      teamWest9: [''],
+      teamWest10: [''],
+      teamWest11: [''],
+      teamWest12: [''],
+      teamWest13: [''],
+      teamWest14: [''],
+      teamWest15: [''],
+      teamWest16: ['']
     });
   }
 
   updateValue() {
-    this.formTeamEast.get("team1").patchValue(this.user.pronostic.classement_SR_east[0]);
-    this.formTeamEast.get("team2").patchValue(this.user.pronostic.classement_SR_east[1]);
-    this.formTeamEast.get("team3").patchValue(this.user.pronostic.classement_SR_east[2]);
-    this.formTeamEast.get("team4").patchValue(this.user.pronostic.classement_SR_east[3]);
-    this.formTeamEast.get("team5").patchValue(this.user.pronostic.classement_SR_east[4]);
-    this.formTeamEast.get("team6").patchValue(this.user.pronostic.classement_SR_east[5]);
-    this.formTeamEast.get("team7").patchValue(this.user.pronostic.classement_SR_east[6]);
-    this.formTeamEast.get("team8").patchValue(this.user.pronostic.classement_SR_east[7]);
+    // teams east
+    this.formTeams.get("teamEast1").patchValue(this.user.pronostic.classement_SR_east[0]);
+    this.formTeams.get("teamEast2").patchValue(this.user.pronostic.classement_SR_east[1]);
+    this.formTeams.get("teamEast3").patchValue(this.user.pronostic.classement_SR_east[2]);
+    this.formTeams.get("teamEast4").patchValue(this.user.pronostic.classement_SR_east[3]);
+    this.formTeams.get("teamEast5").patchValue(this.user.pronostic.classement_SR_east[4]);
+    this.formTeams.get("teamEast6").patchValue(this.user.pronostic.classement_SR_east[5]);
+    this.formTeams.get("teamEast7").patchValue(this.user.pronostic.classement_SR_east[6]);
+    this.formTeams.get("teamEast8").patchValue(this.user.pronostic.classement_SR_east[7]);
+    this.formTeams.get("teamEast9").patchValue(this.user.pronostic.classement_SR_east[8]);
+    this.formTeams.get("teamEast10").patchValue(this.user.pronostic.classement_SR_east[9]);
+    this.formTeams.get("teamEast11").patchValue(this.user.pronostic.classement_SR_east[10]);
+    this.formTeams.get("teamEast12").patchValue(this.user.pronostic.classement_SR_east[11]);
+    this.formTeams.get("teamEast13").patchValue(this.user.pronostic.classement_SR_east[12]);
+    this.formTeams.get("teamEast14").patchValue(this.user.pronostic.classement_SR_east[13]);
+    this.formTeams.get("teamEast15").patchValue(this.user.pronostic.classement_SR_east[14]);
+    this.formTeams.get("teamEast16").patchValue(this.user.pronostic.classement_SR_east[15]);
+
+    // teams west
+    this.formTeams.get("teamWest1").patchValue(this.user.pronostic.classement_SR_west[0]);
+    this.formTeams.get("teamWest2").patchValue(this.user.pronostic.classement_SR_west[1]);
+    this.formTeams.get("teamWest3").patchValue(this.user.pronostic.classement_SR_west[2]);
+    this.formTeams.get("teamWest4").patchValue(this.user.pronostic.classement_SR_west[3]);
+    this.formTeams.get("teamWest5").patchValue(this.user.pronostic.classement_SR_west[4]);
+    this.formTeams.get("teamWest6").patchValue(this.user.pronostic.classement_SR_west[5]);
+    this.formTeams.get("teamWest7").patchValue(this.user.pronostic.classement_SR_west[6]);
+    this.formTeams.get("teamWest8").patchValue(this.user.pronostic.classement_SR_west[7]);
+    this.formTeams.get("teamWest9").patchValue(this.user.pronostic.classement_SR_west[8]);
+    this.formTeams.get("teamWest10").patchValue(this.user.pronostic.classement_SR_west[9]);
+    this.formTeams.get("teamWest11").patchValue(this.user.pronostic.classement_SR_west[10]);
+    this.formTeams.get("teamWest12").patchValue(this.user.pronostic.classement_SR_west[11]);
+    this.formTeams.get("teamWest13").patchValue(this.user.pronostic.classement_SR_west[12]);
+    this.formTeams.get("teamWest14").patchValue(this.user.pronostic.classement_SR_west[13]);
+    this.formTeams.get("teamWest15").patchValue(this.user.pronostic.classement_SR_west[14]);
+    this.formTeams.get("teamWest16").patchValue(this.user.pronostic.classement_SR_west[15]);
   }
 
   onSubmit() {
-    const team1 = this.formTeamEast.get('team1').value;
-    const team2 = this.formTeamEast.get('team2').value;
-    const team3 = this.formTeamEast.get('team3').value;
-    const team4 = this.formTeamEast.get('team4').value;
-    const team5 = this.formTeamEast.get('team5').value;
-    const team6 = this.formTeamEast.get('team6').value;
-    const team7 = this.formTeamEast.get('team7').value;
-    const team8 = this.formTeamEast.get('team8').value;
+    // teams east
+    const teamEast1 = this.formTeams.get('teamEast1').value;
+    const teamEast2 = this.formTeams.get('teamEast2').value;
+    const teamEast3 = this.formTeams.get('teamEast3').value;
+    const teamEast4 = this.formTeams.get('teamEast4').value;
+    const teamEast5 = this.formTeams.get('teamEast5').value;
+    const teamEast6 = this.formTeams.get('teamEast6').value;
+    const teamEast7 = this.formTeams.get('teamEast7').value;
+    const teamEast8 = this.formTeams.get('teamEast8').value;
+    const teamEast9 = this.formTeams.get('teamEast9').value;
+    const teamEast10 = this.formTeams.get('teamEast10').value;
+    const teamEast11 = this.formTeams.get('teamEast11').value;
+    const teamEast12 = this.formTeams.get('teamEast12').value;
+    const teamEast13 = this.formTeams.get('teamEast13').value;
+    const teamEast14 = this.formTeams.get('teamEast14').value;
+    const teamEast15 = this.formTeams.get('teamEast15').value;
+    const teamEast16 = this.formTeams.get('teamEast16').value;
+
+    // teams west
+    const teamWest1 = this.formTeams.get('teamWest1').value;
+    const teamWest2 = this.formTeams.get('teamWest2').value;
+    const teamWest3 = this.formTeams.get('teamWest3').value;
+    const teamWest4 = this.formTeams.get('teamWest4').value;
+    const teamWest5 = this.formTeams.get('teamWest5').value;
+    const teamWest6 = this.formTeams.get('teamWest6').value;
+    const teamWest7 = this.formTeams.get('teamWest7').value;
+    const teamWest8 = this.formTeams.get('teamWest8').value;
+    const teamWest9 = this.formTeams.get('teamWest9').value;
+    const teamWest10 = this.formTeams.get('teamWest10').value;
+    const teamWest11 = this.formTeams.get('teamWest11').value;
+    const teamWest12 = this.formTeams.get('teamWest12').value;
+    const teamWest13 = this.formTeams.get('teamWest13').value;
+    const teamWest14 = this.formTeams.get('teamWest14').value;
+    const teamWest15 = this.formTeams.get('teamWest15').value;
+    const teamWest16 = this.formTeams.get('teamWest16').value;
 
     const teamsEast: string[] = [];
-    teamsEast.push(team1);
-    teamsEast.push(team2);
-    teamsEast.push(team3);
-    teamsEast.push(team4);
-    teamsEast.push(team5);
-    teamsEast.push(team6);
-    teamsEast.push(team7);
-    teamsEast.push(team8);
+    teamsEast.push(teamEast1);
+    teamsEast.push(teamEast2);
+    teamsEast.push(teamEast3);
+    teamsEast.push(teamEast4);
+    teamsEast.push(teamEast5);
+    teamsEast.push(teamEast6);
+    teamsEast.push(teamEast7);
+    teamsEast.push(teamEast8);
+    teamsEast.push(teamEast9);
+    teamsEast.push(teamEast10);
+    teamsEast.push(teamEast11);
+    teamsEast.push(teamEast12);
+    teamsEast.push(teamEast13);
+    teamsEast.push(teamEast14);
+    teamsEast.push(teamEast15);
+    teamsEast.push(teamEast16);
+
+    const teamsWest: string[] = [];
+    teamsWest.push(teamWest1);
+    teamsWest.push(teamWest2);
+    teamsWest.push(teamWest3);
+    teamsWest.push(teamWest4);
+    teamsWest.push(teamWest5);
+    teamsWest.push(teamWest6);
+    teamsWest.push(teamWest7);
+    teamsWest.push(teamWest8);
+    teamsWest.push(teamWest9);
+    teamsWest.push(teamWest10);
+    teamsWest.push(teamWest11);
+    teamsWest.push(teamWest12);
+    teamsWest.push(teamWest13);
+    teamsWest.push(teamWest14);
+    teamsWest.push(teamWest15);
+    teamsWest.push(teamWest16);
 
     const pronos = <Pronostic>({
-      classement_SR_east: teamsEast
+      classement_SR_east: teamsEast,
+      classement_SR_west: teamsWest
     });
 
     this.user.pronostic = pronos;
     this.userService.enregistrerUtilisateur(this.user.uid, this.user);
-
 
     // Redirection vers la vue
     this.router.navigate(['/pronostics']);
@@ -129,7 +235,7 @@ export class RegularSeasonEditComponent implements OnInit {
           confName: team.confName
         });
 
-        t.confName == "East" ? this.teamsEast.push(t) : this.teamWest.push(t)
+        t.confName == "East" ? this.teamsEast.push(t) : this.teamsWest.push(t)
 
       });
     });
